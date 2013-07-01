b52bf54084703c4b8a07b4330bde2145d6af8505
Maxim Penzin <maxp@ya.ru>
2013-07-01 13:50:45 +0900

    initial

---
+++ ethergeig.ino
@@ -0,0 +1,16 @@
+//
+//  ethergeig
+//
+
+#include <EthernetServer.h>
+#include <EthernetClient.h>
+#include <util.h>
+#include <EthernetUdp.h>
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+
+
+
+//.
+
